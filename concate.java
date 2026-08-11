1cae2ba1aa5fd9515039caa1a10a7bfc6927e922
Abi625 <abinayavsb2025@gmail.com>
2026-08-11 09:58:31 +0530

    Add files via upload

---
+++ concate.java
@@ -0,0 +1,7 @@
+public class concate{
+    public static void main(String[] args){
+        String str1 = new String("java");
+        String str2 = new String("java");
+        System.out.println(str1.equals(str2));
+    }
+}
