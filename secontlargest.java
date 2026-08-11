75e92fc5f0f31cdb0324dbe940ccdb0bcb4577c3
Abi625 <abinayavsb2025@gmail.com>
2026-08-11 11:06:56 +0530

    Implement program to find second largest number

---
+++ secontlargest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class secontlargest{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        int secondmax=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }else if(arr[i]>secondmax && arr[i]!=max){
+            secondmax=arr[i];
+        }
+        }
+        System.out.print(secondmax);
+    }
+}
