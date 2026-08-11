92a24d3917914ecd373950609816f67a72d3b165
Abi625 <abinayavsb2025@gmail.com>
2026-08-11 22:07:20 +0530

    Implement Kadane's algorithm for maximum subarray sum

---
+++ kadane's.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class subarray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+          arr[i]=sc.nextInt();
+        }
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum=sum+arr[j];
+            
+        
+        if(sum>maxsum){
+            maxsum=sum;
+        }
+            }
+        }
+        System.out.print(maxsum);
+    }
+}
