5791e769af8713a86d0d2c7d43dbdd319e20c565
Abi625 <abinayavsb2025@gmail.com>
2026-08-27 09:59:48 +0530

    Add common element finder in sameelement.java
    Implement a program to find and print common elements between two arrays.

---
+++ sameelement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class sameelement{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr1[]=new int [n];
+         int arr2[]=new int [m];
+         for(int i=0;i<n;i++){
+              arr1[i]=sc.nextInt();
+         }
+            for(int j=0;j<m;j++){
+               arr2[j]=sc.nextInt();
+         }
+         
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(arr1[i]==arr2[j]){
+                    System.out.print(arr1[i]+" ");
+                } 
+            }
+         }
+    }
+}
