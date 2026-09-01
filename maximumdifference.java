c0ea2bae6cd0b91eaea4041199e3c24848e33da2
Abi625 <abinayavsb2025@gmail.com>
2026-09-01 21:48:27 +0530

    Implement maximum difference calculation

---
+++ maximumdifference.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class maximumdiff{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int diff = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[j]-arr[i]>diff){
+                    diff=arr[j]-arr[i];
+                }
+               
+                    
+                
+            }
+        }
+        System.out.print(diff);
+    }
+}
